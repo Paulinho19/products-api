671e965e0182de999933dfab9e3453c792f8f12f
Paulo Henrique <paulohsantosrocha@gmail.com>
2026-06-22 11:38:04 -0300

    feat: add create product route

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"products-api/model"
 	"products-api/usecase"
 
 	"github.com/gin-gonic/gin"
@@ -26,4 +27,24 @@ func (p *ProductController) GetProducts(ctx *gin.Context){
 	}
 	
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (p *ProductController) CreateProduct(ctx *gin.Context){
+	var product model.Product
+
+	err := ctx.BindJSON(&product)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUseCase.CreateProduct(product)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
 }
